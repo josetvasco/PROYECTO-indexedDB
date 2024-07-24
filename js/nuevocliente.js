5b65e76e865c891e98d60e693e94013670f492d4
josetvasco <torovascojosedaniel@gmail.com>
2024-07-24 18:40:27 -0500

    Se eliminaron unas funciones para pasarlo a otro archivo para poderlo utilizar en otros archivos

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -8,18 +8,6 @@
       formulario.addEventListener('submit', validarCliente)
   });
 
-  function conectarDB() {
-    const abrirConexion = window.indexedDB.open('crm', 1);
-
-    abrirConexion.error = function() {
-      console.log('Hubo un error');
-    }
-
-    abrirConexion.onsuccess = function() {
-      DB = abrirConexion.result;
-    }
-  }
-
   function validarCliente(e) {
     e.preventDefault();
 
@@ -68,29 +56,4 @@
     }
   }
 
-  function imprimirElerta(mensaje, tipo) {
-
-    const alerta = document.querySelector('.alerta');
-
-    if(!alerta) {
-        //Crear la alerta
-      const divMensaje = document.createElement('DIV');
-      divMensaje.classList.add('px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center', 'border', 'alerta');
-
-      if(tipo === 'error') {
-        divMensaje.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
-      } else {
-        divMensaje.classList.add('bg-green-100', 'border-green-400', 'text-green-700');
-      }
-
-      divMensaje.textContent = mensaje;
-
-      formulario.appendChild(divMensaje);
-
-      setTimeout(() => {
-        divMensaje.remove();
-      }, 3000);
-    }
-   
-  }
 })();
